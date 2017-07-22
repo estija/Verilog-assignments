c966ec8e1b42fc4b467bb1237062e017bf2989f5
estija <30321205+estija@users.noreply.github.com>
2017-07-22 22:16:09 +0530

    Create d_ff.v

---
+++ d_ff.v
@@ -0,0 +1,26 @@
+timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/09/2017 07:33:43 PM
+// Design Name: 
+// Module Name: dff
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module dff(input D,clk,output reg Q);
+always@(posedge clk)
+Q=D;
+endmodule
