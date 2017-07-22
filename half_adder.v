2a974974035008956ad14c1cb39455a5da75a515
estija <30321205+estija@users.noreply.github.com>
2017-07-22 22:03:24 +0530

    Update half_adder.v

--- half_adder.v
+++ half_adder.v
@@ -1,3 +1,25 @@
+timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/06/2017 05:20:39 PM
+// Design Name: 
+// Module Name: halfadder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module halfadder(
     input A,
     input B,
