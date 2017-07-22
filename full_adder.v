0186cc8fe9c3387b3a061130936074f731e80e21
estija <30321205+estija@users.noreply.github.com>
2017-07-22 21:59:28 +0530

    Update full_adder.v

--- full_adder.v
+++ full_adder.v
@@ -1,4 +1,26 @@
-module fulladder(input A, input B, input Cin, output S, output Cout);
+timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/07/2017 02:27:13 PM
+// Design Name: 
+// Module Name: fulladder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fulladder(input A,input B,input Cin,output S,output Cout);
     assign S=(A^B)^Cin;
     assign Cout=A&B|B&Cin|Cin&A;
 endmodule
