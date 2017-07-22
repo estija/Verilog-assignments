5432efb1a83c046820b0863293ce78cd6e3b9f5c
estija <30321205+estija@users.noreply.github.com>
2017-07-22 22:00:27 +0530

    Update full-adder.v

--- full-adder.v
+++ full-adder.v
@@ -1,3 +1,25 @@
+timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/07/2017 02:29:08 PM
+// Design Name: 
+// Module Name: testfa
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module testfa;
  reg A, B, Cin;
  wire S, Cout;
