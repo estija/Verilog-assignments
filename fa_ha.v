40d27a4118959dbbc246d56f22fc559895281c50
estija <30321205+estija@users.noreply.github.com>
2017-07-22 21:58:32 +0530

    Create fa_ha.v

---
+++ fa_ha.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/07/2017 03:03:55 PM
+// Design Name: 
+// Module Name: fa
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fa(
+    input A,
+    input B,
+    inout S1,
+    inout Cout1,
+    inout Cout2,
+    input Ci,
+    output S,
+    output Cout
+    );
+    halfadder h1(A, B, S1, Cout1);
+    halfadder h2(Ci, S1, S, Cout2 );
+    assign Cout=Cout1|Cout2;
+endmodule
