378c30eda558ae0f4c660ad36eea6b05cc0abfa4
estija <30321205+estija@users.noreply.github.com>
2017-07-22 22:17:42 +0530

    Create tff_dff2.v

---
+++ tff_dff2.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/09/2017 07:24:37 PM
+// Design Name: 
+// Module Name: tff1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tff1(
+    input clk,T,output A0
+    );
+    t tf1(A0,T,clk);
+endmodule
