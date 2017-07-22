81d193b61c77ac99ec030778d14c73046d1d6513
estija <30321205+estija@users.noreply.github.com>
2017-07-22 22:17:09 +0530

    Create tff_dff1.v

---
+++ tff_dff1.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/09/2017 07:28:51 PM
+// Design Name: 
+// Module Name: t
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module t(output reg Q, input T, clk);
+wire DT=Q^T;
+dff d(T,clk,DT);
+endmodule
